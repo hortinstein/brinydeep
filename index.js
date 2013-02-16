2ed1b8f3273674203ebe9ceeaf657d2f28bbb44b
hortinstein <hortinstein@hortinstein-VirtualBox.(none)>
2013-02-16 18:17:14 +0900

    creating, deleting now work for multiple machines

--- index.js
+++ index.js
@@ -1,49 +1,129 @@
 var request = require('request');
-var argv = require('optimist');
-
+var async = require('async');
 
 var brinydeep = {};
 module.exports = brinydeep;
 
-var c_api_key= "";
-var c_client_id = ""; 
+var api_key= "";
+var client_id = ""; 
+var cred_string = "";
 var host_string = "https://api.digitalocean.com/"
 
-brinydeep.setup = function  (client_id,api_key) {
-	c_api_key = client_id;
-	c_client_id = client_id;
+
+var send_request = function (urls,callback) {
+	//console.log(urls)
+  var req = function (url,callback) {
+  	request(url,function (e,o) {
+			o = JSON.parse(o.body);	
+			if (o.status === 'ERROR') {
+				callback(o.description,o);
+			} else {
+				callback(e,o);	
+			}
+		});
+  }
+	if (Array.isArray(urls)){
+		async.map(urls, req, function (e,r) {
+			callback(e,r);
+		});
+	} else {
+		req(urls,callback);
+	}
+	
 }
 
+var build_requests = function (type, api_path,arg) {
+	if (type === undefined){ type = '/'};
+	if (Array.isArray(arg)){
+		var reqs = [];
+		for (var item in arg){
+			reqs.push(host_string+'/'+type+'/'+arg[item]+'/'+api_path+'/?'+cred_string);
+		}
+		return reqs;
+	} 
+	else return [host_string+'/'+type+'/'+arg+'/'+api_path+'/?'+cred_string];
+}
+
+brinydeep.setup = function  (sclient_id,sapi_key) {
+	api_key = sapi_key;
+	client_id = sclient_id;
+	cred_string = "client_id=" + client_id + "&api_key=" + api_key;
+}
 
 
 /////////////////
 //Documentation//
 /////////////////
-brinydeep.documentation = function  (client_id,api_key,callback) {
-	var req = host_string+ "?client_id=" + client_id 
-		+ "&api_key=" + api_key;
-
+brinydeep.documentation = function  (callback) {
+	var req = host_string+ cred_string;
+	request(req,function (e,o) {
+		o = o.body
+		//console.log(e,o);
+		callback(e,o);
+	});
 };
 
 
 ////////////
 //Droplets//
 ////////////
-brinydeep.show_active = function (client_id,api_key,callback) {
-	
+brinydeep.get_ids = function (callback) {
+	var req = host_string+ "/droplets/?" + cred_string
+	send_request(req,function (e,o) {
+		if (e) { callback(e) }
+		else {
+			var ids = [];
+			o.droplets.forEach(function (machine){
+				ids.push(machine.id);
+			});
+			callback(e,ids);
+		};
+	});
+}
+brinydeep.show_active = function (callback) {
+	var req = host_string+ "/droplets/?" + cred_string
+	send_request(req,callback);
 }
 
-brinydeep.show = function (client_id,api_key,callback) {
+brinydeep.show = function (droplet_ids, callback) {
 	
 };
+
+
 
 // name Required, String, this is the name of the droplet - must be formatted by hostname rules
 // size_id Required, Numeric, this is the id of the size you would like the droplet created at
 // image_id Required, Numeric, this is the id of the image you would like the droplet created with
 // region_id Required, Numeric, this is the id of the region you would like your server in IE: US/Amsterdam
 // ssh_key_ids Optional, Numeric CSV, comma separated list of ssh_key_ids that you would like to be added to the server
+var build_machine_req = function (machine) {
+	var ret = "/new?name="+machine.name+
+													"&size_id="+machine.size_id+
+													"&image_id="+machine.image_id+
+													"&region_id="+machine.region_id+'&'+cred_string;
+
+	if (machine.hasOwnProperty('ssh_key_ids')){
+		ret+=("&ssh_key_ids="+machine.ssh_key_ids);
+	} 
+	return ret;
+}
+
 brinydeep.new_droplet = function (options,callback) {
-	
+	var new_machine_req = "";
+	if (options.hasOwnProperty('droplets')){
+		var new_machine_req = [];
+		options.droplets.forEach(function (machine){
+			//console.log(machine)
+			new_machine_req.push( build_machine_req(machine));
+		});
+	} else {
+		new_machine_req = build_machine_req(options);
+	}
+	options = build_requests('droplets','/',new_machine_req);
+	console.log(options);
+	send_request(options,callback);
+
+
 };
 // droplet_id(s) Required, Integer, this is the id of your droplet
 brinydeep.reboot = function (droplet_ids,callback) {
@@ -94,10 +174,22 @@ brinydeep.enable_automatic_backups = function (droplet_ids,callback) {
 brinydeep.disable_automatic_backups = function (droplet_ids,callback) {
 	
 };
+//image_id Required, Numeric, this is the id of the image you would like to use to rebuild your droplet with
+brinydeep.destroy_all_droplets = function (callback) {
+	brinydeep.get_ids(function (e,o) {
+		if (!e){
+			var reqs = build_requests('droplets','destroy',o)
+			send_request(reqs,callback)
+		}
+	})
+};
+
 //droplet_id(s) Required, Integer, this is the id of your droplet
 brinydeep.destroy = function (droplet_ids,callback) {
-	
+	var reqs = build_requests('droplets','destroy',droplet_ids);
+	send_request(reqs,callback)
 };
+
 ///////////
 //Regions//
 ///////////
@@ -118,6 +210,8 @@ brinydeep.show_images = function (image_id, callback) {
 brinydeep.destroy_images = function (image_id, callback) {
 	
 };
+
+
 ////////////
 //SSH Keys//
 ////////////
@@ -138,5 +232,10 @@ brinydeep.show_ssh_key = function (ssh_key_id,callback) {
 ////////Coming soon with API///////////////////////////////////
 
 brinydeep.sizes = function (callback) {
-	
+	var req = host_string+ "/sizes/?" + cred_string
+	send_request(req,callback);
+}
+
+if(!module.parent) {
+
 }
--- test/test.js
+++ test/test.js
@@ -1,6 +1,82 @@
 var should = require('should');
-var config = require('../../config.json')
+var config = require('../../config.json');
+var brinydeep = require('../index.js');
+
 before(function (done) {
-	
+		brinydeep.setup(config.client_key, config.api_key);
+		done();
 });
-it('should be able to show all active droplets')
+
+var new_droplet_multi = {
+	droplets: [
+		{
+			name: "test2",
+			size_id: 66,
+			image_id: 25306,
+			region_id: 1
+		},
+		{
+			name: "test3",
+			size_id: 66,
+			image_id: 25306,
+			region_id: 1
+		}
+	]
+};
+var new_droplet = {
+	name: "test1",
+	size_id: 66,
+	image_id: 25306,
+	region_id: 1
+};
+var id;
+// it('should be able to show documentation', function (done) {
+// 	brinydeep.documentation(function (e,o) {
+// 		o.should.be.a('string');
+// 		done();
+// 	});
+// });
+
+// it('should be able to show active droplets', function (done) {
+// 	brinydeep.show_active(function (e,o) {
+// 		o.status.should.equal('OK');
+// 		o.droplets.length.should.equal(0);
+// 		done();
+// 	});
+// });
+
+// it('should be able to create a droplet', function (done) {
+// 	brinydeep.new_droplet(new_droplet,function (e,o) {
+// 		o.status.should.equal('OK');
+// 		done();
+// 	});
+// });
+// it('should be able to create multiple droplets', function (done) {
+// 	brinydeep.new_droplet(new_droplet_multi,function (e,o) {
+// 		console.log(e,o)
+// 		o.status.should.equal('OK');
+// 		o.droplets.length.should.equal(0);
+// 		done();
+// 	});
+// });
+// it('should be able to list all ids', function (done) {
+// 	brinydeep.get_ids(function (e,o) {
+// 		console.log(e,o)
+// 		o.length.should.equal(3);
+// 		done();
+// 	});
+// });
+it('should be able to destroy all machines', function (done) {
+	brinydeep.destroy_all_droplets(function (e,o) {
+		o.forEach(function (stat) {
+			stat.status.should.equal('OK');
+		})
+		done();
+	});
+});
+it('should be able to list sizes', function (done) {
+	brinydeep.sizes(function (e,o) {
+		o.status.should.equal('OK');
+		done();
+	});
+});
